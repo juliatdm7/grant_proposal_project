70ff18eb4936856234086f230fed6b114c56e336
juliatdm7 <juliatdm7@gmail.com>
2024-12-06 13:48:06 +0100

    Clarifying explanations

--- predator_prey_model.R
+++ predator_prey_model.R
@@ -3,8 +3,8 @@
 # Defining parameters
 params <- list(
   r = 1.06,      # Prey growth rate just above 1
-  K = 100,      # Carrying capacity
-  a = 2,     # Attack rate (searching efficiency)
+  K = 100,      # Carrying capacity (no particular interest in setting a specific value)
+  a = 2,     # H erbivores’ pressure on R. okamurae (attack rate or searching efficiency; relatively high as R. okamurae is quite abundant now. However, it would be interesting to make it so that it varies according to how N changes)
   h = 3,      # Handling time (in days)
   k = 0.095,
   f_h = 0.6,      # Predator efficiency
