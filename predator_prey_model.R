165470f8521f41082d655d2130f6f0257445d3e6
juliatdm7 <juliatdm7@gmail.com>
2024-12-06 13:56:11 +0100

    Elaboration of figure

--- predator_prey_model.R
+++ predator_prey_model.R
@@ -1,4 +1,5 @@
 ## Predator-Prey Model ###
+library(ggplot2)
 
 # Defining parameters
 params <- list(
@@ -17,31 +18,31 @@ nsteps <- 15
 # Storage for population sizes
 pop.df <- data.frame(time=1:nsteps,N=numeric(nsteps),H=numeric(nsteps))
 pop.df <- within(pop.df,{
-  N[1] <- 20
+  N[1] <- 20  # Population of R. okamurae initially considered to be relatively low
   H[1] <- 50
 })
 
 # Discrete model simulation with functional response
 for (t in 2:nsteps) {
   pop.df <- within(pop.df,{
-    # Type II Functional Response
+    # Type II Functional Response (herbivory pressure)
     G <- (params$a * N[t-1] * H[t-1]) / (1 + params$a * N[t-1] * params$h)
     
-    # Prey population (logistic growth with functional response grazing)
+    # Prey population (logistic growth with functional response of herbivorous grazing)
     N[t] <- max(0, N[t-1] + params$r * N[t-1]*(1-N[t-1]/params$K) - G) 
     
-    # Predator population (growth, baseline mortality, toxicity-driven mortality)
+    # Predator population (growth, baseline mortality and toxicity-driven mortality)
     H[t] <- max(0, H[t-1] + params$f_h * G - params$d  * H[t-1] * (params$d + params$k * N[t-1])) 
   })
 }
 
 # Plot results
-library(ggplot2)
 
 ggplot(pop.df, aes(x = time)) +
   geom_line(aes(y = N, color = "Prey (R. okamurae)")) +
   geom_line(aes(y = H, color = "Herbivores (P. lividus etc.)")) +
   labs(title = "Discrete Predator-Prey Model with Type II Functional Response",
        x = "Time", y = "Population Size") +
-  scale_color_manual(values = c("blue", "red")) +
+  scale_color_manual(name = "Legend", 
+                     values = c("blue", "red")) +
   theme_minimal()
