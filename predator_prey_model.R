19fdaf0f453dd6efec91db8e6f3b2edf453b9ba5
juliatdm7 <juliatdm7@gmail.com>
2024-12-06 13:50:57 +0100

    Further commenting

--- predator_prey_model.R
+++ predator_prey_model.R
@@ -5,8 +5,8 @@ params <- list(
   r = 1.06,      # Prey growth rate just above 1
   K = 100,      # Carrying capacity (no particular interest in setting a specific value)
   a = 2,     # H erbivores’ pressure on R. okamurae (attack rate or searching efficiency; relatively high as R. okamurae is quite abundant now. However, it would be interesting to make it so that it varies according to how N changes)
-  h = 3,      # Handling time (in days)
-  k = 0.095,
-  f_h = 0.6,      # Predator efficiency
+  h = 3,      # Time (in days) that takes for an herbivore to consume a whole R. okamurae individual (high due to deterrent effects of SM)
+  k = 0.095,   # Additional predator mortality due to toxic effects, proportional to prey abundance.
+  f_h = 0.6,      # Predator fertility (proportion of offspring that thrives; intermediate value)
   d = 0.1      # Baseline predator death rate
 )
